5bb66d0401ee60c9da2e16390bc3243ef460d605
Nila2812 <knila2812@gmail.com>
2025-11-07 10:50:35 +0530

    Update adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -37,3 +37,22 @@ export const deleteAdmin = async (req, res) => {
     res.status(500).json({ error: "Failed to delete admin" });
   }
 };
+
+
+//Login admin
+export const loginAdmin = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ username });
+    if (!admin) return res.status(401).json({ error: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) return res.status(401).json({ error: "Invalid credentials" });
+
+    res.status(200).json({ message: "Login successful", adminId: admin._id });
+  } catch (err) {
+    console.error("Login error:", err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
